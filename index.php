1533a0c31f1733cef90b0cf7c327ab7939d3b27e
Milan Darjanin <milan.darjanin@gmail.com>
2013-06-06 15:43:18 +0200

    more test cases in index.php. changes in form.php

--- index.php
+++ index.php
@@ -7,19 +7,50 @@
 </head>
 <body>
 	<!-- TODO: move styles to own file. -->
-<div class="pure-g" style="margin: 0 auto; width: 640px;">
+<div class="pure-g" style="margin: 0 auto; width: 960px;">
 	<div class="pure-u-1">
 <?php 
 include('lib/database.php');
 include('lib/form.php');
 
-$form_input = new Form('post', '','Testing form', true);
+$form_input = new Form('post', '','Testing form', false, true);
 $form_input->input('name');
-$form_input->input('date_age', 'Date Age');
-echo $form_input->generate();
+$form_input->input('date_of_birth', 'Date of Birth');
+$form_input->input_password('password');
+$form_input->input_password('password_again');
+$form_input->checkbox('terms', 'Do you agree with the terms?');
+$form_input->button('Sign in', 'submit');
+echo $form_input->generate(false);
+
+$recept_form = new Form('post', $_SERVER['PHP_SELF'], 'Pridaj novy recept', false, true);
+$recept_form->input('recept');
+$recept_form->input('time', 'Cas pripravy');
+$recept_form->button('Pridaj recept', 'submit');
+echo $recept_form->generate(false);
+
+$db = new Database('localhost', 'varime', '', 'varime');
+$db->connect();
+
+if (isset($_POST['recept'])) {
+	$data = array('id' => null, 'recept' => $_POST['recept'], 'time' => $_POST['time']);
+	unset($_POST['recept']);
+	if ($db->insert('test', $data)) {
+	}
+	header("Location: ".$_SERVER['PHP_SELF']);
+}
+
+echo "<table class='pure-table'>";
+echo "<thead><tr><td>id</td><td>recept</td><td>time</td></tr><thead><tbody>";
+foreach ($db->all('test') as $row) {
+	echo "<tr>";
+		echo "<td>".$row['id']."</td><td>".$row['recept']."</td><td>".$row['time']." min</td>";
+	echo "</tr>";
+}
+echo "</tbody></table>";
+
+unset($db);
  ?>
 	 </div>
  </div>
-
 </body>
 </html>
--- lib/form.php
+++ lib/form.php
@@ -3,8 +3,16 @@ class Form {
 	var $form, $method;
 
 	// TODO: send more arguments in dictionary. method and action basic lvl
-	public function __construct($method = 'post', $action = '', $legend = null, $stacked='true') {
-		$class = $stacked ? "pure-form pure-form-stacked" : "pure-form";
+	public function __construct($method = 'post', $action = '', $legend = null, $stacked = true, $aligned = true) {
+
+		$class = "pure-form";
+		if ($stacked) {
+			$class .= " pure-form-stacked";
+		}
+		if ($aligned) {
+			$class .= " pure-form-aligned";	
+		}
+
 		$this->form = "<form method='$method' action='$action' class='$class'><fieldset>";
 		if (isset($legend)) {
 			$this->form .= "<legend>$legend</legend>";
@@ -12,16 +20,56 @@ class Form {
 	}
 
 	public function input($name, $label = null, $type = 'text') {
-		if (!isset($label)) {
-			$label = $name;
-		}
-		$this->form .= "<label for='$name'>$label</label>";
-		$this->form .=  "<input type='$type' id='$name' placeholder='$label'/>";
+		$label = $this->label_text($name, $label);
+
+		$tmp = "<div class='pure-control-group'>";
+		$tmp .= "<label for='$name'>$label</label>";
+		$tmp .= "<input type='$type' name='$name' id='$name' placeholder='$label'/>";
+		$tmp .= "</div>";
+
+		$this->add_to_form($tmp);
 	}
 
-	public function generate() {
+	public function checkbox($name, $label = null, $type = 'text') {
+		$label = $this->label_text($name, $label);
+
+		$tmp = "<div class='pure-controls'>";
+		$tmp .= "<label for='$name' class='pure-checkbox'>";
+		$tmp .= "<input type='checkbox' name='$name' id='$name'/>$label</label>";
+		$tmp .= "</div>";
+
+		$this->add_to_form($tmp);
+	}
+
+	public function input_password($name, $label = null) {
+		$this->input($name, $label, 'password');
+	}
+
+	public function button($label = 'Submit', $type = 'submit') {
+		$tmp = "<div class='pure-controls'>";
+		$tmp .= "<button type='$type' name=submit class='pure-button pure-button-primary' value='$label'>$label</button></div>";
+
+		$this->add_to_form($tmp);
+	}
+
+	public function generate($submit_button = true) {
+		if ($submit_button) {
+			$this->button();
+		}
+
 		$this->form .= "</fieldset></form>";
 		return $this->form;
+	}
+
+	private function add_to_form($string) {
+		$this->form .= $string;
+	}
+
+	private function label_text($name, $label) {
+		if (!isset($label)) {
+			return ucwords(str_replace('_',' ',$name));
+		}
+		return $label;
 	}
 
 }
